604c0741790bd9729ea9226194a7172bbfece228
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-09-20 23:21:40 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import web
+from collections import deque
+
+messages = deque([])
+
+urls = (
+    '/(.*)', 'notification'
+)
+app = web.application(urls, globals())
+
+
+class notification:
+    def GET(self, url):
+        if len(messages) > 0:
+            return messages.popleft()
+
+        return ''
+
+    def POST(self, url):
+        message = web.data()  # you can get data use this method
+        messages.append(message)
+        return 'ok'
+
+if __name__ == "__main__":
+    app.run()
