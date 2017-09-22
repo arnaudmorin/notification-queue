a946f4831e8ff6ed708f62bf5bbe51e537afb6e8
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-09-22 11:43:55 +0200

    sdfkl

--- read.sh
+++ read.sh
@@ -2,7 +2,7 @@
 
 while true ; do
 	sleep 1
-	data=$(curl http://145.239.194.85:8080 2>/dev/null)
+    data=$(curl http://notifications.arnaudmorin.fr:8080/$(cat ~/.p_notif) 2>/dev/null)
 	if [ "Z$data" != "Z" ]; then
 		notify-send "$data"
 		blink.sh &
