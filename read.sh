a52a8115b0ce4ff3ef8a5d848836f21b906aebe5
Arnaud Morin <arnaud.morin@gmail.com>
2018-08-31 14:05:22 +0200

    Add SMS notif
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- read.sh
+++ read.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 while true ; do
-	sleep 1
+    sleep 1
     data=$(curl http://notifications.arnaudmorin.fr:8080/$(cat ~/.p_notif) 2>/dev/null)
-	if [ "Z$data" != "Z" ]; then
-		notify-send "$data"
-		blink.sh &
-	fi
+    if [ "Z$data" != "Z" ]; then
+        notify-send "$data"
+        blink.sh &
+        curl -X  POST -d to=33618671034 -d message="salut" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
+    fi
 done
