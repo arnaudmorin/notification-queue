a7c8b9b1818df9be6d4ede8501b3b2abd2380676
Arnaud Morin <arnaud.morin@gmail.com>
2019-06-13 15:56:00 +0200

    Add echo
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- read.sh
+++ read.sh
@@ -4,8 +4,9 @@ while true ; do
     sleep 1
     data=$(curl http://notifications.arnaudmorin.fr:8080/$(cat ~/.p_notif) 2>/dev/null)
     if [ "Z$data" != "Z" ]; then
+        echo $data
         notify-send "$data"
         blink.sh &
-        curl -X  POST -d to=33618671034 -d message="$data" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
+        #curl -X  POST -d to=33618671034 -d message="$data" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
     fi
 done
