5f8e7aee0dcfaf383ff302e4dc5768f65c564736
Arnaud Morin <arnaud.morin@gmail.com>
2019-09-16 10:28:40 +0200

    Open links
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- read.sh
+++ read.sh
@@ -4,9 +4,15 @@ while true ; do
     sleep 1
     data=$(curl https://notifications.arnaudmorin.fr/$(cat ~/.p_notif) 2>/dev/null)
     if [ "Z$data" != "Z" ]; then
-        echo $data
-        notify-send "$data"
-        blink.sh &
-        #curl -X  POST -d to=33618671034 -d message="$data" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
+        # If it's a link to open
+        if [[ $data =~ ^xdg-open.* ]]; then
+            echo "Link: $data"
+            eval $data
+        else
+            echo "Message: $data"
+            notify-send "$data"
+            blink.sh &
+            #curl -X  POST -d to=33618671034 -d message="$data" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
+        fi
     fi
 done
