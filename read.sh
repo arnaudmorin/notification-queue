b8087c87ef3a4d6589b05e468bf82b48cc92f1f1
Arnaud Morin <arnaud.morin@gmail.com>
2019-12-02 10:00:42 +0100

    Add blink guirlance
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- read.sh
+++ read.sh
@@ -13,6 +13,7 @@ while true ; do
             echo "Message: $data"
             notify-send "$data"
             blink.sh &
+            blink_guirlande.sh &
             #curl -X  POST -d to=33618671034 -d message="$data" -d token="$(cat ~/.p_sms)" http://192.168.100.3:5000/send
         fi
     fi
